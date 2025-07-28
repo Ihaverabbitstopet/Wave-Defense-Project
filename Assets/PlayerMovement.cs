ab47e76bb2b23c2bc82eb0e9433b2a0bf029166e
William <wfchen30@gmail.com>
2025-07-28 15:17:14 -0400

    reworking all player inputs

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,33 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    public float speed = 5f;
+    public float moveSpeed = 5f;
+    private Vector2 moveInput;
     private Rigidbody2D rb;
-    private float horizontal;
-    private float vertical;
-    // Start is called before the first frame update
-    void Start()
+
+    private PlayerControls controls;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        controls.Player.Enable();
+        controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+    }
+
+    private void OnDisable()
+    {
+        controls.Player.Disable();
+    }
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-         horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
-
-
-    }
-
-    void FixedUpdate()
-    {
-        Vector2 movement = new Vector2(horizontal, vertical).normalized;
-        rb.velocity = movement * speed;
+        rb.velocity = moveInput * moveSpeed;
     }
 }
