981ac7882ce7308c8b7aaf2b92c259253550640a
William <wfchen30@gmail.com>
2025-07-29 15:51:39 -0400

    added player movement animations. Working on adding attack

---
+++ Assets/EnemyHealth.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth = 3;
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        Debug.Log($"{gameObject.name} took {amount} damage. Remaining HP: {currentHealth}");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        // Optional: play death animation
+        Destroy(gameObject);
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,36 +5,78 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
     public float moveSpeed = 5f;
     private Vector2 moveInput;
+
+    [Header("References")]
+    public GameObject attackHitbox;        // Assign AttackHitbox here
+    public Transform playerVisual;         // Assign PlayerVisual here
     private Rigidbody2D rb;
+    private Animator animator;
 
     private PlayerControls controls;
+    private bool isAttacking;
 
-    private void Awake()
+    void Awake()
     {
+        rb = GetComponent<Rigidbody2D>();
+        animator = playerVisual.GetComponent<Animator>();
         controls = new PlayerControls();
     }
 
-    private void OnEnable()
+    void OnEnable()
     {
-        controls.Player.Enable();
-        controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
-        controls.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+        controls.Enable();
+        controls.Player.Attack.performed += ctx => HandleAttack();
     }
 
-    private void OnDisable()
+    void OnDisable()
     {
-        controls.Player.Disable();
+        controls.Player.Attack.performed -= ctx => HandleAttack();
+        controls.Disable();
     }
 
-    private void Start()
+    void Update()
     {
-        rb = GetComponent<Rigidbody2D>();
+        moveInput = controls.Player.Move.ReadValue<Vector2>();
+
+        // Flip based on mouse position
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        Vector3 scale = transform.localScale;
+        scale.x = (mouseWorldPos.x < transform.position.x) ? -1f : 1f;
+        transform.localScale = scale;
+
+        // Set animation speed
+        float speed = moveInput.magnitude;
+        animator.SetFloat("Speed", speed);
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        rb.velocity = moveInput * moveSpeed;
+        if (!isAttacking)
+        {
+            rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+        }
     }
+
+    private void HandleAttack()
+    {
+        if (!isAttacking)
+            StartCoroutine(PerformAttack());
+    }
+
+    private System.Collections.IEnumerator PerformAttack()
+    {
+        isAttacking = true;
+        animator.SetTrigger("Attack");
+        yield return new WaitForSeconds(0.5f); // Adjust to match animation length
+        isAttacking = false;
+    }
+
+    // Called from animation events
+    public void EnableHitbox() => attackHitbox.SetActive(true);
+    public void DisableHitbox() => attackHitbox.SetActive(false);
 }
+
+
